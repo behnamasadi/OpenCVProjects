9dec307e5b8c6bca5ff1cb0198ad830f5d45918f
Behnam Asadi <behnam.asadi@gmail.com>
2023-08-20 23:29:54 +0200

    ls slam updated

--- scripts/least_square_slam.py
+++ scripts/least_square_slam.py
@@ -1,6 +1,8 @@
 # export PATH="/home/$USER/usr/bin:$PATH"
 # export LD_LIBRARY_PATH="/home/$USER/usr/lib:$LD_LIBRARY_PATH"
 
+# ref: https://github.com/goldbattle/simple_2d_slam/
+
 import g2o_file_reader
 from load_2d_g2o import load_2d_g2o
 import os
@@ -15,37 +17,42 @@ np.set_printoptions(threshold=sys.maxsize)
 
 def getNode(nodes, id):
     for node in nodes:
-        # print(node["id"])
-        # print(id)
+
         if node["id"] == id:
             return node
 
 
 def wrap2pi(theta):
-    while theta.all() > math.pi:
+    while theta > math.pi:
         theta = theta - 2*math.pi
 
-    while theta.all() < -math.pi:
+    while theta < -math.pi:
         theta = theta + 2*math.pi
     return theta
 
 
 def rot2(theta):
-    R = np.zeros(2, 2)
+    R = np.zeros([2, 2])
 
     R = np.array([[np.cos(theta), -np.sin(theta)],
                  [np.sin(theta), np.cos(theta)]])
     return R
 
 
+def update_node(node, dx):
+
+    # position is just an addition
+    node["state"][0] = node["state"][0] + dx[0]
+    node["state"][1] = node["state"][1] + dx[1]
+
+    # add theta, then enforce it is within -pi to pi
+    node["state"][2] = wrap2pi(node["state"][2] + dx[2])
+
+    return node
+
+
 g2o_file = os.path.abspath("./data/slam/input_INTEL_g2o.g2o")
 nodes, edges = load_2d_g2o(filename=g2o_file)
-# print(nodes[1]["id"])
-# print(nodes[1]["state"])
-# # print(nodes)
-# print(type(nodes))
-# print(type(nodes[1]["state"]))
-# print(type(nodes["state"]))
 
 
 x_cords = [node["state"][0] for node in nodes]
@@ -61,83 +68,138 @@ plt.grid(True)
 
 # ensure our orientations are bounded between -pi and pi
 
-theta = np.array(theta)
-
-wrap2pi(theta)
-# print(theta)
-
-# for t in theta:
-#     print(t)
-
-# print(np.array(theta) / math.pi)
 
 dx_norm = np.inf
 iteration = 0
-# print(len(x_cords))
 
-print("edges[0]:", edges[0])
-# print(nodes[0])
+A_i_j = np.zeros([3, 3])
+B_i_j = np.zeros([3, 3])
 
-# {'id1': 0, 'id2': 1, 'meas': [0.0, 0.0, -0.000642], 'info': [[11.111271, -0.249667, 0.0], [-0.249667, 399.99984, 0.0], [0.0, 0.0, 2496.793089]]}
+while dx_norm > 1e-2:
+    # our matrices for Hx=-b
+    b = np.zeros([3*len(x_cords), 1])
+    H = np.zeros([3*len(x_cords), 3*len(x_cords)])
 
-for edge in edges:
+    for edge in edges:
 
-    # print(id1)
-    # print(getNode(nodes, id1))
-    node1 = getNode(nodes, id1)
-    node2 = getNode(nodes, id2)
+        # idout
+        id1 = edge["id1"]
+        # idin
+        id2 = edge["id2"]
 
-    x1 = node1["state"][0]
-    y1 = node1["state"][1]
-    theta1 = node1["state"][2]
+        node1 = getNode(nodes, id1)
+        node2 = getNode(nodes, id2)
 
-    x2 = node2["state"][0]
-    y2 = node2["state"][1]
-    theta2 = node2["state"][2]
+        x1 = node1["state"][0]
+        y1 = node1["state"][1]
+        theta1 = node1["state"][2]
 
-    theta2_in_1 = theta2-theta1
+        x2 = node2["state"][0]
+        y2 = node2["state"][1]
+        theta2 = node2["state"][2]
 
-    # orientation error
-    err_theta = wrap2pi(
-        edge["meas"][3] - wrap2pi(node2["state"][3]-node1["state"][3]))
+        theta2_in_1 = wrap2pi(theta2-theta1)
 
-    # position error
-    R_1toG = rot2(node1["state"][3])
-    p_1inG = node1["state"][1:2]
-    p_2inG = node2["state"][1:2]
-    p_2in1 = np.matmul(np.transpose(R_1toG), (p_2inG - p_1inG))
-    p_2in1_from_measurement = edge["meas"][1:2]
-    err_pos = p_2in1_from_measurement - p_2in1
+        # orientation error
+        err_theta = wrap2pi(wrap2pi(edge["meas"][2]) - theta2_in_1)
 
-    # Jacobian of current relative in respect to NODE 1
+        # position error
+        R_1toG = rot2(theta1)
 
-    A_i_j = np.zeros(3, 3)
-    A_i_j = [[-np.cos(theta1), -np.sin(theta1), -np.sin(theta1)(x2-x1) + np.cos(theta1)(y2-y1)],
-             [np.sin(theta1), -np.cos(theta1), -np.cos(theta1)
-              (x2-x1) - np.sin(theta1)(y2-y1)],
-             [0, 0, -1]]
+        p_1inG = np.array([[x1], [y1]])
+        p_2inG = np.array([[x2], [y2]])
 
-    # Jacobian of current relative in respect to NODE 2
+        p_2in1 = R_1toG.T @ (p_2inG - p_1inG)
 
-    B_i_j = np.zeros(3, 3)
-    B_i_j = [[np.cos(theta1), -np.sin(theta2), 0],
-             [-np.sin(theta1), np.cos(theta1), 0], [0, 0, 1]]
+        p_2in1_from_measurement = np.array(
+            [[edge["meas"][0]], [edge["meas"][1]]])
 
-    # update our information
-    id1 = edge["id1"]
-    id2 = edge["id2"]
-    # H(3*id1+1:3*id1+3,3*id1+1:3*id1+3) = H(3*id1+1:3*id1+3,3*id1+1:3*id1+3) + Aij'*edge.info*Aij;
-    # H(3*id1+1:3*id1+3,3*id2+1:3*id2+3) = H(3*id1+1:3*id1+3,3*id2+1:3*id2+3) + Aij'*edge.info*Bij;
-    # H(3*id2+1:3*id2+3,3*id1+1:3*id1+3) = H(3*id2+1:3*id2+3,3*id1+1:3*id1+3) + Bij'*edge.info*Aij;
-    # H(3*id2+1:3*id2+3,3*id2+1:3*id2+3) = H(3*id2+1:3*id2+3,3*id2+1:3*id2+3) + Bij'*edge.info*Bij;
+        err_pos_x, err_pos_y = p_2in1_from_measurement - p_2in1
 
-    # update our error terms
-    # b(3*id1+1:3*id1+3,1) = b(3*id1+1:3*id1+3,1) + Aij'*edge.info*[err_pos; err_theta];
-    # b(3*id2+1:3*id2+3,1) = b(3*id2+1:3*id2+3,1) + Bij'*edge.info*[err_pos; err_theta];
+        err_pos_x = err_pos_x.squeeze()
+        err_pos_y = err_pos_y.squeeze()
+
+        # 3x3d data on the file is I11 I12 I13 I22 I23 I33 but the matrix is symmetric so
+        info = np.array(edge["info"])
+
+        # Jacobian of current relative in respect to NODE 1
+
+        A_i_j = np.array([[-np.cos(theta1), -np.sin(theta1), -np.sin(theta1)*(x2-x1) + np.cos(theta1)*(y2-y1)],
+                          [np.sin(theta1), -np.cos(theta1), -np.cos(theta1) *
+                           (x2-x1) - np.sin(theta1)*(y2-y1)],
+                          [0, 0, -1]])
+
+        # print("A_i_j:", A_i_j)
+
+        # Jacobian of current relative in respect to NODE 2
+
+        B_i_j = np.array([[np.cos(theta1), -np.sin(theta2), 0],
+                          [-np.sin(theta1), np.cos(theta1), 0], [0, 0, 1]])
+
+        # print("B_i_j:", B_i_j)
+
+        # update our information
+
+        # H_i_i
+        H[3*id1:3*id1+3, 3*id1:3*id1+3] = H[3*id1:3 *
+                                            id1+3, 3*id1:3*id1+3] + A_i_j.T @ info@A_i_j
+
+        # print("H_i_i:", H[3*id1:3*id1+3, 3*id1:3*id1+3])
+
+        # H_i_j
+        H[3*id1:3*id1+3, 3*id2:3*id2+3] = H[3*id1:3 *
+                                            id1+3, 3*id2:3*id2+3] + A_i_j.T @ info @ B_i_j
+
+        # print("H_i_j:", H[3*id1:3*id1+3, 3*id2:3*id2+3])
+
+        # H_j_i
+        H[3*id2:3*id2+3, 3*id1: 3*id1+3] = H[3*id2:3 *
+                                             id2+3, 3*id1: 3*id1+3]+B_i_j.T@info@A_i_j
+
+        # print("H_j_i:", H[3*id2:3*id2+3, 3*id1: 3*id1+3])
+
+        # H_j_j
+        H[3*id2:3*id2+3, 3*id2: 3*id2+3] = H[3*id2:3 *
+                                             id2+3, 3*id2: 3*id2+3]+B_i_j.T@info@B_i_j
+
+        # print("H_j_j:", H[3*id2:3*id2+3, 3*id2: 3*id2+3])
+
+        # update our error terms
+        result = A_i_j.T @ info @ np.array([[err_pos_x],
+                                            [err_pos_y], [err_theta]])
+
+        # print(result)
+
+        result = result.squeeze()
+
+        b[3*id1:3*id1+3, 0] = b[3*id1:3*id1+3, 0] + result
+
+  #      print("b", b[3*id1:3*id1+3, 0])
+
+        result = B_i_j.T @ info @ np.array([[err_pos_x],
+                                           [err_pos_y], [err_theta]])
+        result = result.squeeze()
+
+ #       print(result)
+
+        b[3*id2:3*id2+3, 0] = b[3*id2:3*id2+3, 0] + result
+
+#        print("b", b[3*id2:3*id2+3, 0])
 
 
-# while dx_norm > 1e-2:
-#     #our matrices for Hx=-b
-#     b = np.zeros(3*len(x_cords), 1)
-#     H = np.zeros(3*len(x_cords), 3*len(x_cords))
-#     pass
+#  fix the first node to be known
+    H[0:3, 0:3] = H[0:3, 0:3] + 1e6*np.eye(3)
+    # solve the linear system
+    # x = H\b
+    x = np.linalg.solve(H, -b)
+
+    print(x)
+    print(x.shape)
+
+    dx_norm = np.linalg.norm(x)
+    iteration = iteration + 1
+    print(f'iter {iteration} with delta = {dx_norm}\n', iteration, dx_norm)
+
+    # update our nodes
+    for index, node in enumerate(nodes):
+        node = update_node(node, x[3*index:3*index+3, 0])
